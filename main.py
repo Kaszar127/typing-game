dda4adc0728467f95db2d6f44d95cd1ccaf62eb5
SilasCh <silas.b.christiansen@gmail.com>
2022-03-29 12:13:42 +0200

    Changes resolution, title and icon

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from pygame.locals import *
 pygame.init()
 
 pygame.display.set_caption("Type or DIE")
+pygame.display.set_icon(pygame.image.load("C:/Users/silas/OneDrive/Billeder/typing.gif"))
 
-resolution = (700,400)
+resolution = (1280,720)
 white = (255,255,255)
 
 screen = pygame.display.set_mode(resolution)

1bf70bed0c89d26004fb94b936d377b4735fea1e
SilasCh <silas.b.christiansen@gmail.com>
2022-03-29 11:51:50 +0200

    program running

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import pygame
+from pygame.locals import *
+
+pygame.init()
+
+pygame.display.set_caption("Type or DIE")
+
+resolution = (700,400)
+white = (255,255,255)
+
+screen = pygame.display.set_mode(resolution)
+
+while True:
+    screen.fill(white)
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()

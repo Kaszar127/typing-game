9fd5adff93408ea8f8039766daf9b484aef8802b
SilasCh <silas.b.christiansen@gmail.com>
2022-03-29 12:40:23 +0200

    importing pygame as pg

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-import pygame
+import pygame as pg
 from pygame.locals import *
 
-pygame.init()
+pg.init()
 
-pygame.display.set_caption("Type or DIE")
-pygame.display.set_icon(pygame.image.load("C:/Users/silas/OneDrive/Billeder/typing.gif"))
+pg.display.set_caption("Type or DIE")
+pg.display.set_icon(pg.image.load("C:/Users/silas/OneDrive/Billeder/typing.gif"))
 
-resolution = (1280,720)
+resolution = (700,400)
 white = (255,255,255)
 
-screen = pygame.display.set_mode(resolution)
+screen = pg.display.set_mode(resolution,pg.RESIZABLE)
 
 while True:
     screen.fill(white)
-    pygame.display.flip()
+    pg.display.flip()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()

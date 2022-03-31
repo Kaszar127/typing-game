d26475be8ec0ddc5c598f8d462935ba3fa31a809
SilasCh <88002368+Kaszarr@users.noreply.github.com>
2022-03-31 12:37:40 +0200

    Assigned text to enemy class and cleaned up wordlist

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import pygame as pg
-
 from pygame import gfxdraw
+import random
 
-class Ball:
-    def __init__(self, xPos, yPos, xVel = 1, yVel = 1, rad = 15):
-        self.x = xPos
-        self.y = yPos
-        self.dx = xVel
-        self.dy = yVel
+class Enemy:
+    def __init__(self, xPos, yPos, words, rad = 15):
+        self.x = int(xPos)
+        self.y = int(yPos)
         self.radius = rad
         self.type = "ball"
+        self.word = random.choice(words)
+
     def draw(self, surface):
-        gfxdraw.filled_circle(surface, int(self.x), int(self.y), self.radius, black)
-        gfxdraw.aacircle(surface, int(self.x), int(self.y), self.radius, black)
+        gfxdraw.filled_circle(surface, self.x, self.y, self.radius, black)
+        gfxdraw.aacircle(surface, self.x, self.y, self.radius, black)
+        
+        text = font.render(self.word, True, black)
+        screen.blit(text,(self.x, self.y - 100))
 
 # Makes wordlist.txt into an actual list
 wordfile = open("Assets/wordlist.txt", "r")
 worddata = wordfile.read()
 wordlist = worddata.split("\n")
-print(wordlist)
 wordfile.close()
 
 pg.init()
+
+font = pg.font.Font('Assets/Roboto-Black.ttf',18)
 
 pg.display.set_caption("Type or DIE")
 pg.display.set_icon(pg.image.load("Assets/typing.jpg"))
@@ -30,14 +34,15 @@ resolution = (700,400)
 white = pg.Color(255,255,255)
 black = pg.Color(0,0,0)
 
-screen = pg.display.set_mode(resolution,pg.RESIZABLE)
+screen = pg.display.set_mode(resolution)
 
-ball = Ball(resolution[0]/2, resolution[1]/2)
+Enemy = Enemy(resolution[0]/2, resolution[1]/2, wordlist)
 
 while True:
     screen.fill(white)
 
-    ball.draw(screen)
+    Enemy.draw(screen)
+    
     
     pg.display.flip()
     

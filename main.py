230143b15f351bfa12a5ad8ae6b1e3e5a44d294a
SilasCh <88002368+Kaszarr@users.noreply.github.com>
2022-03-31 14:10:09 +0200

    Added user input and comparing input to object word

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ class Enemy:
         text = font.render(self.word, True, black)
         screen.blit(text,(self.x, self.y - 100))
 
+    def elim(self, user_text):
+        if self.word == user_text:
+            print("banan")
+
 # Makes wordlist.txt into an actual list
 wordfile = open("Assets/wordlist.txt", "r")
 worddata = wordfile.read()
@@ -33,19 +37,31 @@ pg.display.set_icon(pg.image.load("Assets/typing.jpg"))
 resolution = (700,400)
 white = pg.Color(255,255,255)
 black = pg.Color(0,0,0)
+user_text = ''
 
 screen = pg.display.set_mode(resolution)
 
-Enemy = Enemy(resolution[0]/2, resolution[1]/2, wordlist)
+enemy = Enemy(resolution[0]/2, resolution[1]/2, wordlist)
 
 while True:
     screen.fill(white)
 
-    Enemy.draw(screen)
-    
+    enemy.draw(screen)
     
     pg.display.flip()
     
     for event in pg.event.get():
+        if event.type == pg.KEYDOWN:
+            user_text += event.unicode
+            if event.key == pg.K_BACKSPACE:
+                user_text = user_text[:-2]
+            if event.key == pg.K_SPACE:
+                user_text = user_text[:-1]
+                print((user_text))
+                enemy.elim(user_text)
         if event.type == pg.QUIT:
             pg.quit()
+
+#    for char in range(len(user_text)):
+#       if user_text[char] == enemy.word:
+#          print(user_text[char])

8aaaf0d8aef2c43333821ca75a2f51ac14f36b42
Kaszarr <silas.b.christiansen@gmail.com>
2022-05-02 23:06:54 +0200

    Final cleanup

--- main.py
+++ main.py
@@ -8,19 +8,16 @@ import os.path
 class Enemy:
     def __init__(self, surface, words, speed):
         self.surface = surface
-        self.randomVar = random.randint(0,360)
         self.spawnRad = int(self.surface.get_width()/2)
+        self.randomVar = random.randint(0,360)
         self.position = pg.Vector2(int(self.surface.get_width()/2 + self.spawnRad * math.cos(self.randomVar * math.pi / 180)), int(self.surface.get_height()/2 + self.spawnRad * math.sin(self.randomVar * math.pi / 180)))
         self.radius = 15
         self.fontSize = 22
-        # Will eventually self-destruct because of index out range
+        # Will eventually self-destruct because of index range
         self.word = words.pop(words.index(random.choice(words)))
         self.enemySpeed = speed
         
     def draw(self):
-        #gfxdraw.filled_circle(self.surface, int(self.position.x), int(self.position.y), self.radius, pg.Color(255,0,0))
-        #gfxdraw.aacircle(self.surface, int(self.position.x), int(self.position.y), self.radius, pg.Color(255,0,0))
-        
         # Displays enemy word
         screen.blit(font1.render(self.word, True, pg.Color(255,0,0)),(self.position.x - self.radius/2 - (self.fontSize*len(self.word)/5), self.position.y - (userRadius*3) - (self.fontSize/2)))
 
@@ -30,23 +27,26 @@ class Enemy:
         else:
             screen.blit(slimeSprite,(self.position.x - screen.get_width()/30, self.position.y - screen.get_height()/30))
 
+    def move(self):
+        # Creates direction vector from enemy position to the center
+        self.target = (pg.Vector2(self.surface.get_size())/2 - self.position).normalize()
+        
+        # Moves the enemy with direction vector and speed
+        self.velocity = pg.Vector2(self.target.x * self.enemySpeed, self.target.y * self.enemySpeed)
+        self.position += self.velocity
+    
+    def isColliding(self):
+        # Checks if enemy and wizard is colliding
+        length = (pg.Vector2(self.surface.get_size())/2 - self.position).length()
+        
+        # Returns true or false
+        return (length <= float(self.radius))
+    
     def elim(self, user_text):
+        # Checks if enemy word is the same as input
         if(self.word == user_text):
             return True, 
         return False
-
-    def move(self):
-        try:
-            self.target = (pg.Vector2(self.surface.get_size())/2 - self.position).normalize()
-        except:
-            pass
-
-        self.velocity = pg.Vector2(self.target.x * self.enemySpeed, self.target.y * self.enemySpeed)
-        self.position += self.velocity
-
-    def isColliding(self):
-        length = (pg.Vector2(self.surface.get_size())/2 - self.position).length()
-        return (length <= float(self.radius))
 
 # Various variables
 resolution = [1000, 1000]
@@ -107,27 +107,22 @@ while True:
         screen.blit(grassSprite,(0, 0))
         screen.blit(wizardSprite,(screen.get_width()/2-screen.get_width()/20, screen.get_height()/2-screen.get_height()/20))
     
-        # Calls enemy draw method
+        # Calls enemy draw function
         list(map(lambda x: x.draw(), enemyList))
 
-        # Calls enemy move method based on deltatime
+        # Calls enemy move function and isColliding function based on deltatime
         if(timer >= 0.01):
             for i in range(len(enemyList)):
                 enemyList[i].move()
-                if (enemyList[i].isColliding()):
-                    playing = not enemyList[i].isColliding()
-
+                if(enemyList[i].isColliding()):
+                    playing = False
             timer = 0
 
-        # Generates enemy based on deltatime
+        # Generates enemy based on deltatime and changes enemy speed
         if(enemyTimer >= 2):
             enemyList.append(Enemy(screen, wordlist, speed))
             speed += 0.02
             enemyTimer = 0
-
-        # For visulisation will remove
-        # gfxdraw.aacircle(screen,int(screen.get_width()/2),int(screen.get_height()/2),int(resolution[0]/2),pg.Color(0,0,0))
-        # gfxdraw.aacircle(screen,int(screen.get_width()/2),int(screen.get_height()/2),userRadius,pg.Color(0,0,0))
 
         # Displays score and user input
         screen.blit(font2.render("Score: " + str(score), True, pg.Color(0,0,0)),(screen.get_width()/2 - (36*(len("Score: ")+len(str(score)))/5), 36))
@@ -139,12 +134,15 @@ while True:
         # Main event loop
         for event in pg.event.get():
             if(event.type == pg.KEYDOWN):
+                # Puts the clicked button in a string
                 user_text += event.unicode
                 if(event.key == pg.K_BACKSPACE):
+                    # Removes the backspace unicode character and the previous typed character
                     user_text = user_text[:-2]
                 if(event.key == pg.K_SPACE or event.key == pg.K_RETURN):
+                    # Removes space or enter unicode
                     user_text = user_text[:-1]
-                
+    
                     # Created to avoid overunning enemyList while reffering to the last element
                     indexList = []
                     for i in range(len(enemyList)):
@@ -153,7 +151,8 @@ while True:
                             score += len(user_text)*10
                     for j in range(len(indexList)):
                         enemyList.pop(indexList[j])
-                
+
+                    # Resets user input
                     user_text = ""
             if(event.type == pg.QUIT):
                 pg.quit()
@@ -178,6 +177,7 @@ while True:
         for event in pg.event.get():
             if(event.type == pg.KEYDOWN):
                 if(event.key == pg.K_RETURN):
+                    # Resets game variables 
                     score = 0
                     enemyList = []
                     user_text = ""
